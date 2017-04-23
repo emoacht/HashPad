dcfd6d6be461d13b6c92c6979b481e61ab4e527b
emoacht <emotom@pobox.com>
2017-04-23 10:48:02 +0900

    Incremented minor version

--- Source/FileHashChecker/Properties/AssemblyInfo.cs
+++ Source/FileHashChecker/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("File Hash Checker")]
-[assembly: AssemblyCopyright("Copyright © 2015 EMO")]
+[assembly: AssemblyCopyright("Copyright © 2015-2017 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
