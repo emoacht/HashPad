1d97d3300534ba00201e07a5c13201f1db0a7313
emoacht <emotom@pobox.com>
2020-09-05 10:00:26 +0900

    Redesign window

--- Source/FileHashChecker/Views/MainWindow.xaml.cs
+++ Source/FileHashChecker/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,27 +19,74 @@ namespace FileHashChecker.Views
 {
 	public partial class MainWindow : Window
 	{
+		#region Property
+
+		public bool IsMenuOpen
+		{
+			get { return (bool)GetValue(IsMenuOpenProperty); }
+			set { SetValue(IsMenuOpenProperty, value); }
+		}
+		public static readonly DependencyProperty IsMenuOpenProperty =
+			DependencyProperty.Register(
+				"IsMenuOpen",
+				typeof(bool),
+				typeof(MainWindow),
+				new PropertyMetadata(false));
+
+		#endregion
+
 		private readonly MainWindowViewModel _mainWindowViewModel;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			_mainWindowViewModel = this.DataContext as MainWindowViewModel;
-			if (_mainWindowViewModel != null)
-			{
-				this.Loaded += OnLoaded;
-				this.Drop += OnDrop;
+			_mainWindowViewModel = (MainWindowViewModel)this.DataContext;
 
-				this.SetBinding(
-					AllowDropProperty,
-					new Binding(nameof(MainWindowViewModel.IsReading))
-					{
-						Source = _mainWindowViewModel,
-						Mode = BindingMode.OneWay,
-						Converter = new BooleanInverseConverter()
-					});
-			}
+			this.SetBinding(
+				AllowDropProperty,
+				new Binding(nameof(MainWindowViewModel.IsReading))
+				{
+					Source = _mainWindowViewModel,
+					Mode = BindingMode.OneWay,
+					Converter = new BooleanInverseConverter()
+				});
+
+			this.Loaded += OnLoaded;
+			this.Drop += OnDrop;
+		}
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			this.TitleBar.MouseDown += (_, e) =>
+			{
+				e.Handled = true;
+
+				if (IsMenuOpen)
+					IsMenuOpen = false;
+				else
+					this.DragMove();
+			};
+			this.MenuPain.MouseDown += (_, e) => { e.Handled = true; };
+			this.MouseDown += (_, e) => { IsMenuOpen = false; };
+		}
+
+		protected override void OnStateChanged(EventArgs e)
+		{
+			// Prevent Aero Snap.
+			if (WindowState == WindowState.Maximized)
+				WindowState = WindowState.Normal;
+
+			base.OnStateChanged(e);
+		}
+
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			base.OnClosing(e);
+
+			_mainWindowViewModel.Cancel();
 		}
 
 		private async void OnLoaded(object sender, RoutedEventArgs e)
@@ -53,6 +101,41 @@ namespace FileHashChecker.Views
 			var paths = ((DataObject)e.Data).GetFileDropList().Cast<string>();
 
 			await _mainWindowViewModel.CheckFileAsync(paths);
+		}
+
+		private void Menu_Click(object sender, RoutedEventArgs e)
+		{
+			IsMenuOpen = !IsMenuOpen;
+		}
+
+		private void Minimize_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = WindowState.Minimized;
+		}
+
+		private void Close_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private async void Browse_Click(object sender, RoutedEventArgs e)
+		{
+			await _mainWindowViewModel.SelectFileAsync();
+		}
+
+		private void Clipboard_Click(object sender, RoutedEventArgs e)
+		{
+			_mainWindowViewModel.ReadClipboard();
+		}
+
+		private async void Compute_Click(object sender, RoutedEventArgs e)
+		{
+			await _mainWindowViewModel.ComputeHashValuesAsync();
+		}
+
+		private void Stop_Click(object sender, RoutedEventArgs e)
+		{
+			_mainWindowViewModel.Cancel();
 		}
 	}
 }
