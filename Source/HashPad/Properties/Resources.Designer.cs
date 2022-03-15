bb339e131620b8ec19c4e5b0a0871520fef8e724
emoacht <emotom@pobox.com>
2022-03-15 23:13:23 +0900

    Add aggregated license

--- Source/HashPad/Properties/Resources.Designer.cs
+++ Source/HashPad/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace HashPad.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -142,7 +142,7 @@ namespace HashPad.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://github.com/emoacht/HashPad/blob/master/LICENSE.txt.
+        ///   Looks up a localized string similar to https://github.com/emoacht/HashPad/blob/master/License.md.
         /// </summary>
         public static string LicenseUrl {
             get {
